25dc151f742a852af8267307635f39ce54086258
James Robinson <mrmandrill@gmail.com>
2014-05-07 08:18:13 +0100

    Create greenBottles.py

---
+++ greenBottles.py
@@ -0,0 +1,4 @@
+print("10 green bottles, hanging on the wall")
+print("10 green bottles, hanging on the wall")
+print("And if 1 green bottle should acidentally fall,")
+print("They'll be 9 green bottles hanging on the wall.")
