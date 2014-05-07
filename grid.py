6cc10f72a262c4d0fa4f02bba0ed9e8098727b49
Mr Awesome <sshiers7040@sohamcollege.org.uk>
2014-05-07 09:39:43 +0100

    Update grid.py
    Stored the grid in a 2-dimensional list and then looped to print each line.

--- grid.py
+++ grid.py
@@ -1,4 +1,4 @@
-print("-------")
-print("| | | |")
-print("-------")
-print("| | | |")
+grid = [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0] #To store each space
+
+for x in range(0, 5, 1): #Loop for each line
+    print(grid[x]) #Print the line
