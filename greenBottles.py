3952ab5ee16b7074a308183241837eae33fedc44
sshiers7040 <sshiers7040@sohamcollege.org.uk>
2014-05-07 09:32:21 +0100

    Update greenBottles.py
    It loops with time pauses.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,15 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+#X Green Bottles
+#Program counts down from a given number to 0 with the lines of 10 green bottles.
+import time #Allows pauses
+
+bottles = int(input("How many green bottles?")) #How many bottles
+
+for x in range(bottles,0,-1): #Repeat until no bottles left
+    print("{0} green bottle hanging on the wall.".format(x))
+    time.sleep(2)
+    print("{0} green bottle hanging on the wall.".format(x))
+    time.sleep(2)
+    print("And if 1 green bottle should acidentally fall,")
+    time.sleep(2)
+    print("There'll be {0} green bottles hanging on the wall.\n".format(x-1))
+    time.sleep(3)
